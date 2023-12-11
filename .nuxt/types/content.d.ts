1dbb4d9e05ec95669a8808c944eae328c5f2c06d
syafiqhadzir <syafiqhadzir@live.com.my>
2023-12-11 11:49:00 +0800

    Upload Docus

---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('/home/ssd-workspace/tutor-page/docs/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
+  const parseContent: typeof import('/home/ssd-workspace/tutor-page/docs/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
